3391e1ca0a7b5c09e115d3bc6b32ba4a8a269bcc
ebesnoin <ebesnoin@student.42.fr>
2024-04-18 21:41:22 +0200

    [ADD] toupper and tolower

--- libft.h
+++ libft.h
@@ -29,6 +29,8 @@ int		ft_isalpha(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+int		ft_tolower(int c);
+int		ft_toupper(int c);
 int		atoi(const char *nptr);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
